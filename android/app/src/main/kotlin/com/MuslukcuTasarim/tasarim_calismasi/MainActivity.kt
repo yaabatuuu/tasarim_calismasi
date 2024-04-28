78628cc231ca4960280584884860cf107e258587
Muslukcu <77172048+yaabatuuu@users.noreply.github.com>
2024-04-28 13:25:40 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/MuslukcuTasarim/tasarim_calismasi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.MuslukcuTasarim.tasarim_calismasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/anasayfa.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pizza"),
+        backgrou
+      ), //AppBar
+      body: const Center(),
+      ),
+  }
+}
